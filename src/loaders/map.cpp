f5aa3ab7dc0fcc2648404f032258e29cee084a13
Aaron O'Mullan <aaron.omullan@gmail.com>
2015-11-01 22:14:29 +0100

    Add src/loaders/map

---
+++ src/loaders/map.cpp
@@ -0,0 +1,50 @@
+#include "map.h"
+
+#include "../tools/dirs.h"
+
+// Internal funcs
+Map *ParseMap(const std::string &data);
+const char *CharToTile(char ch);
+
+MapLoader::MapLoader(const std::string &prefix) {
+    this->prefix = prefix;
+}
+
+std::vector<std::string> MapLoader::List() {
+    return DirList(this->prefix);
+}
+
+Map *MapLoader::Load(const std::string &name) {
+    auto data = FileReadString(this->prefix+name);
+    return ParseMap(data);
+}
+
+Map *ParseMap(const std::string &data) {
+    auto lines = StringLines(data);
+
+    TileMap tiles;
+    for(auto &line : lines) {
+        TileLine tileLine;
+        for(auto &ch : StringChars(line)) {
+            tileLine.push_back(CharToTile(ch));
+        }
+        tiles.push_back(tileLine);
+    }
+
+    return new Map();
+}
+
+// Converts a level map's char to it's corresponding texture/object
+const char *CharToTile(char ch) {
+    switch(ch) {
+        case '.':
+            return "background";
+        case 'W':
+            return "water";
+        case 'M':
+            return "mario_start";
+        case 'B':
+            return "bridge";
+    }
+    return "background";
+}
---
+++ src/loaders/map.h
@@ -0,0 +1,22 @@
+#ifndef MAPLOADER_H
+#define MAPLOADER_H
+
+#include <string>
+#include <vector>
+
+#include "../game/map.h"
+#include "../tools/files.h"
+#include "../tools/strings.h"
+
+class MapLoader {
+    std::string prefix;
+
+public:
+    MapLoader(const std::string &prefix);
+    // Load a specific map
+    Map *Load(const std::string &name);
+    // List available maps
+    std::vector<std::string> List();
+};
+
+#endif
