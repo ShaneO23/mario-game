4bb9cd9f7afa9eef8162aedcd440c969d1b8c24b
Aaron O'Mullan <aaron.omullan@gmail.com>
2015-11-02 01:27:09 +0100

    New main.cpp using new code

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include "game.h"
+#include "IUTSDL.h"
+
+#define TITRE "Mario GEII"
+
+// Dimensions de la grille
+#define NBL 20          // Nombre de lignes
+#define NBC 35          // Nombre de colonnes
+
+// Dimensions des cases : correspond au images bitmap
+#define CASE_W 34       // Largeur
+#define CASE_H 34       // Hauteur
+
+// Dimensions de l'écran
+#define SCREEN_W NBC*CASE_W
+#define SCREEN_H NBL*CASE_H
+
+int main(int argc, char* argv[]) {
+    // Window
+    SDL_Window   *pScreen=NULL;
+    // Video Buffer
+    SDL_Renderer *sdlRenderer=NULL;
+
+    // Init SDL with feature: video, timer
+    if( IUTSDL_Init( SDL_INIT_VIDEO | SDL_INIT_TIMER) != NO_ERROR )
+    {
+        printf("Error: IUTSDL\n") ;
+        exit(1) ;
+    }
+
+    // Create Window
+    pScreen = IUTSDL_CreateWindow(TITRE, SCREEN_W, SCREEN_H);
+    if (pScreen == NULL) {
+        printf("Error: IUTSDL\n") ;
+        exit(1) ;
+    }
+
+    // Setup renderer
+    //sdlRenderer = SDL_CreateRenderer(pScreen, -1, 0 /*SDL_RENDERER_ACCELERATED*/);
+    sdlRenderer = SDL_CreateRenderer(pScreen, -1, SDL_RENDERER_SOFTWARE);
+
+    // Clear video buffer
+    IUTSDL_ClearScreen(sdlRenderer);
+
+    // Setup game
+    Game *game = new Game(sdlRenderer);
+
+    // Run game
+    game->Run();
+}
