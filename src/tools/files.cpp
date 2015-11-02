d362bd084a7dc5ac7ffaaf88cabe61b40ff20cc0
Aaron O'Mullan <aaron.omullan@gmail.com>
2015-11-02 01:25:58 +0100

    Fix FileReadString func

--- src/tools/files.cpp
+++ src/tools/files.cpp
@@ -1,6 +1,6 @@
 #include "files.h"
 
 std::string FileReadString(const std::string& name) {
-  std::ifstream fstream("myfile.txt");
+  std::ifstream fstream(name);
   return std::string((std::istreambuf_iterator<char>(fstream)), (std::istreambuf_iterator<char>()));
 }
