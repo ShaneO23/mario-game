dca21354a98ac62cb042e4d202e55eca7ca541d9
Aaron O'Mullan <aaron.omullan@gmail.com>
2016-01-01 22:09:57 +0100

    Fallback to software renderer if hardware doesn't work

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,10 @@ int main(int argc, char* argv[])
 
     // Setup renderer
     sdlRenderer = SDL_CreateRenderer(pScreen, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    //sdlRenderer = SDL_CreateRenderer(pScreen, -1, SDL_RENDERER_SOFTWARE);
+    if(sdlRenderer == NULL) {
+        // Fallback to software only if hardware acceleration is not available
+        sdlRenderer = SDL_CreateRenderer(pScreen, -1, SDL_RENDERER_SOFTWARE);
+    }
 
     // Nicely rescale the renderer
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
