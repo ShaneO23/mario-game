190adaa6fe6820ce44f4c9a45424d9de61ef415d
Aaron O'Mullan <aaron.omullan@gmail.com>
2016-01-06 21:57:08 +0100

    Fixup main.cpp
    Conflicts shane caused

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,7 @@
 #include "game.h"
-<<<<<<< HEAD
-#include "IUTSDL.h"
-#include "Windows.h"
-//Titre de la fenetre
-#define TITRE "Shane Mario Game"
-=======
 #include "IUTSDL.h"
 
 #define TITRE "Shane's Mario Game GEII"
->>>>>>> origin/master
 
 // Dimensions de la grille
 #define NBL 20          // Nombre de lignes
@@ -20,67 +13,7 @@
 
 // Dimensions de l'écran
 #define SCREEN_W NBC*CASE_W
-<<<<<<< HEAD
 #define SCREEN_H NBL*CASE_H
-
-#define MOVE_LEFT    0
-#define MOVE_RIGHT   1
-#define MOVE_UP      2
-#define MOVE_DOWN    3
-
-
-char gTitle[100] = "";
-
-
-Uint32 callback( Uint32 interval, void* param )
-{
-    static int compteur = 0 ;
-
-    compteur+=interval;
-    int seconds;
-    int total_time =60;
-    int game_time;
-    int rest_time =0;
-    // Computer time
-    SYSTEMTIME t;
-    GetSystemTime(&t);
-
-    seconds=compteur / 1000 ;
-    game_time=seconds;
-    game_time -=4;
-    rest_time=total_time - game_time;
-
-        if (total_time<seconds)
-        {
-            sprintf(gTitle,"NO Time left !!");
-            SDL_Delay(1000);
-            printf(gTitle,"The is going to close in %d second !",rest_time);
-            SDL_Delay(1000);
-            exit(0);
-        }
-
-
-
-    sprintf(gTitle,"%d: %d: %d  Game time: %d",t.wHour+1,t.wMinute,t.wSecond,seconds);
-
-    return interval;
-}
-=======
-#define SCREEN_H NBL*CASE_H
-
-char gTitle[100] = "";
-
-Uint32 callback( Uint32 interval, void* param )
-{
-    static int compteur = 0 ;
->>>>>>> origin/master
-
-    compteur+=interval;
-
-    sprintf(gTitle,"Compteur : %d ...", compteur);
-
-    return interval;
-}
 
 int main(int argc, char* argv[]) {
     // Window
