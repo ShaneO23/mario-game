040cb34ea59f31b17cb3a9ccc04328c7789aef26
Aaron O'Mullan <aaron.omullan@gmail.com>
2016-01-07 01:22:02 +0100

    Circling bombs

--- src/objects/bomb.h
+++ src/objects/bomb.h
@@ -13,11 +13,21 @@ struct Bombe : Object {
         return "bombe";
     }
 
+    Sprite Render() {
+        return Sprite(
+            "bombe",
+            this->X+5 + 50*cos(t/50/(2*3.14)),
+            this->Y+5 + 50*sin(t/50/(2*3.14)),
+            34,
+            34
+        );
+    }
+
     // Adjust bounding rect for sprite
     Rect BoundingRect() {
         return Rect(
-            this->X+5,
-            this->Y+5,
+            this->X+10 + 50*cos(t/50/(2*3.14)),
+            this->Y+10 + 50*sin(t/50/(2*3.14)),
             20,
             20
         );
