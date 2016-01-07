837d54067fb6463654824ee1b46f9f5f9b05d053
ShaneO23 <shane@wifi-cp-244-99.unilim.fr>
2016-01-07 11:11:20 +0100

    flagpole.h

--- src/objects/flagpole.h
+++ src/objects/flagpole.h
@@ -1,6 +1,16 @@
-#ifndef FLAGPOLE_H_INCLUDED
-#define FLAGPOLE_H_INCLUDED
+#ifndef OBJECTS_FLAGPOLE_H
+#define OBJECTS_FLAGPOLE_H
 
+#include "./types.h"
 
+struct Flagpole : Object {
+    Flagpole() {
+        this->Width = 34;
+        this->Height = 34;
+    }
+
+    const char *Type() {
+        return "flagpole";
+    }
 
 #endif // FLAGPOLE_H_INCLUDED
